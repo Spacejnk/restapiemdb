aa5289b5d5216c704f55e1155b15943369da3eb0
Timits <will4f21@yahoo.com>
2020-04-30 05:09:17 -0700

    test routes post with postman added body-parser edit readme

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
 
 
+app.use(bodyParser.json());
+// import routes 
+const postRoute = require('./routes/post');
+
+// use this post route midd/ware
+app.use('/post', postRoute);
 
 
 
 // connect to db
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true } , () => console.log('connected to DB!'))
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to DB!'))
 
 app.listen(3000);
--- routes/post.js
+++ routes/post.js
@@ -1,3 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post')
+
+router.get('/', (req, res) => {
+    res.send('Test the post');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('other post');
+});
+
+module.exports = router;
