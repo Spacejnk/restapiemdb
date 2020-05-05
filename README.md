4ca2e708a4a9556aeff981616b4c72c718426557
Timits <will4f21@yahoo.com>
2020-05-05 03:45:32 -0700

    added cors patch update

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 *  express
 *  mongodb
 *  body-parser
+* cors
--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // import routes 
 const postRoute = require('./routes/post');
 
 // use this post route midd/ware
+app.use(cors());
 app.use('/post', postRoute);
 
-
+// routes
+app.get('/', (req, res) => {
+    res.send('Get test');
+})
 
 // connect to db
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to DB!'))
